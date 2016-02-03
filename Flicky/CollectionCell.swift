8483cc3c9b02fdfd979e85ffd2435af484a0d849
venegu <venegu@gmail.com>
2016-02-02 23:01:45 -0500

    Adding early stare collection view

---
+++ Flicky/CollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionCell.swift
+//  Flicky
+//
+//  Created by Gale on 2/2/16.
+//  Copyright © 2016 Gale. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+
+
+}
---
+++ Flicky/CollectionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CollectionViewController.swift
+//  Flicky
+//
+//  Created by Gale on 2/2/16.
+//  Copyright © 2016 Gale. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionCell", forIndexPath: indexPath) as! CollectionCell
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
