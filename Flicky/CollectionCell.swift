316c95b8d1e2237a05517d344d5e9d078ddbaced
venegu <venegu@gmail.com>
2016-02-03 22:30:29 -0500

    Working collection view at last

--- Flicky/CollectionCell.swift
+++ Flicky/CollectionCell.swift
@@ -2,13 +2,13 @@
 //  CollectionCell.swift
 //  Flicky
 //
-//  Created by Gale on 2/2/16.
+//  Created by Gale on 2/3/16.
 //  Copyright © 2016 Gale. All rights reserved.
 //
 
 import UIKit
 
 class CollectionCell: UICollectionViewCell {
-
-
+    
+    @IBOutlet weak var movieView: UIImageView!
 }
--- Flicky/CollectionViewController.swift
+++ Flicky/CollectionViewController.swift
@@ -11,12 +11,48 @@ import UIKit
 class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies: [NSDictionary]?
+    var filteredMovies: [NSDictionary]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        filteredMovies = movies
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(
+            URL: url!,
+            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
+            timeoutInterval: 10)
+        
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate: nil,
+            delegateQueue: NSOperationQueue.mainQueue()
+        )
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            print("response: \(responseDictionary)")
+                            
+                            self.movies = (responseDictionary["results"] as! [NSDictionary])
+                            self.filteredMovies = self.movies
+                            self.collectionView.reloadData()
+                            
+                    }
+                }
+        })
+        task.resume()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,14 +61,53 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        if let filteredMovies = filteredMovies {
+            return filteredMovies.count
+            
+        } else {
+            return 0
+        }
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionCell", forIndexPath: indexPath) as! CollectionCell
+        let movie = filteredMovies![indexPath.row]
+        
+        let baseUrl = "http://image.tmdb.org/t/p/w150"
+        if let posterPath = movie["poster_path"] as? String {
+            let imageUrl = NSURL(string: baseUrl + posterPath)
+            let imageRequest = NSURLRequest(URL: imageUrl!)
+            
+            // Fading in the image!
+            cell.movieView.setImageWithURLRequest(imageRequest, placeholderImage: nil, success: { (imageRequest, imageResponse, image) -> Void in
+                if imageResponse != nil {
+                    print("Image was not cached, fade in image")
+                    cell.movieView.alpha = 0.0
+                    cell.movieView.image = image
+                    UIView.animateWithDuration(0.3, animations: { () -> Void in
+                        cell.movieView.alpha = 1.0
+                    })
+                } else {
+                    print("Image was cached so just update the image")
+                    cell.movieView.image = image
+                }
+                },
+                failure: { (imageRequest, imageResponse, error) -> Void in
+                    // if we have a network error ... show nothing?
+            })
+            
+            
+        } else {
+            // no image
+            cell.movieView.image = nil
+            
+        }
+
         
         return cell
     }
+    
+    
     
 
     /*
