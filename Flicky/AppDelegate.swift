8870a9a94dbfd680399e5ab838bf617f660231ed
venegu <venegu@gmail.com>
2016-02-03 22:46:05 -0500

    Making navigation buttons white

--- Flicky/AppDelegate.swift
+++ Flicky/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Making navigation buttons white
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
         return true
     }
 
